e4e82507d2e27d47d07a29f34265c1f6a4a9831c
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-14 16:35:02 -0500

    start main view

---
+++ src/pages/ListShop.tsx
@@ -0,0 +1,13 @@
+function ListShop() {
+	return (
+		<>
+			<div className="bg-black w-screen h-screen">
+				<h1 className="text-white text-5xl">
+					Hola! Soy la vista de la lista de la tienda :D
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default ListShop;
