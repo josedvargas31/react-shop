2c6f7b71d03cd25299ebbaf6d1462058c83ac247
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-15 16:53:08 -0500

    Add dynamically render

--- src/mocks/MockShop.tsx
+++ src/mocks/MockShop.tsx
@@ -1,6 +1,6 @@
 import { ShopModel } from "../models/ShopModel";
 
-export const Shop: ShopModel[] = [
+export const shops: ShopModel[] = [
     {
         nombre: 'Pan Uno',
         precio: 21.22000,
@@ -9,11 +9,11 @@ export const Shop: ShopModel[] = [
     {
         nombre: 'Pan Dos',
         precio: 121.220,
-        img: './vite.svg'
+        img: '/vite.svg'
     },
     {
         nombre: 'Pan Tres',
         precio: 221.222,
-        img: './vite.svg'
+        img: '/vite.svg'
     },
 ]
--- src/pages/ListShop.tsx
+++ src/pages/ListShop.tsx
@@ -1,3 +1,4 @@
+import { shops } from "../mocks/MockShop";
 function ListShop() {
 	return (
 		<>
@@ -5,6 +6,15 @@ function ListShop() {
 				<h1 className="text-white text-4xl">
 					Hola! Soy la vista de la lista de la tienda :D
 				</h1>
+				<div className="grid sm:grid-cols-2  xs:grid-cols-1 gap-8 place-content-between">
+					{shops.map((item) => (
+						<div key={item.nombre}>
+							<h3 className="text-white">{item.nombre}</h3>
+							<p className="text-white">${item.precio}</p>
+							<img className="w-20 h-20" src={item.img} alt={item.nombre} />
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	);
