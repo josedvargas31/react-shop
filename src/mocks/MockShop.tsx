e83a3d9f494629116ea853844dc255eafcd3c08c
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-15 20:17:43 -0500

    Add styles & images

--- src/mocks/MockShop.tsx
+++ src/mocks/MockShop.tsx
@@ -1,19 +1,44 @@
 import { ShopModel } from "../models/ShopModel";
 
 export const shops: ShopModel[] = [
-    {
-        nombre: 'Pan Uno',
-        precio: 21.22000,
-        img: './vite.svg'
-    },
-    {
-        nombre: 'Pan Dos',
-        precio: 121.220,
-        img: '/vite.svg'
-    },
-    {
-        nombre: 'Pan Tres',
-        precio: 221.222,
-        img: '/vite.svg'
-    },
-]
+	{
+		nombre: "Pan pequeño",
+		precio: 500,
+		img: "./imgs/break-small.jpg",
+	},
+	{
+		nombre: "Pan mediano",
+		precio: 1000,
+		img: "./imgs/break-small.jpg",
+	},
+	{
+		nombre: "Pan grande",
+		precio: 2000,
+		img: "./imgs/break-big.jpg",
+	},
+	{
+		nombre: "Pan extra grande",
+		precio: 3000,
+		img: "./imgs/break-bigger.jpg",
+	},
+	{
+		nombre: "cruasán",
+		precio: 2000,
+		img: "./imgs/break-cruasan.jpg",
+	},
+	{
+		nombre: "Pan roscon dulce",
+		precio: 1000,
+		img: "./imgs/break-roscon.jpg",
+	},
+	{
+		nombre: "Cucas",
+		precio: 500,
+		img: "./imgs/break-cuca.jpg",
+	},
+	{
+		nombre: "Torta recortada",
+		precio: 2000,
+		img: "./imgs/break-uno.jpg",
+	},
+];
--- src/pages/ListShop.tsx
+++ src/pages/ListShop.tsx
@@ -2,18 +2,31 @@ import { shops } from "../mocks/MockShop";
 function ListShop() {
 	return (
 		<>
-			<div className="bg-black w-full h-screen md:px-40 xs:px-6">
-				<h1 className="text-white text-4xl">
-					Hola! Soy la vista de la lista de la tienda :D
-				</h1>
-				<div className="grid sm:grid-cols-2  xs:grid-cols-1 gap-8 place-content-between">
-					{shops.map((item) => (
-						<div key={item.nombre}>
-							<h3 className="text-white">{item.nombre}</h3>
-							<p className="text-white">${item.precio}</p>
-							<img className="w-20 h-20" src={item.img} alt={item.nombre} />
-						</div>
-					))}
+			<div className="bg-zinc-100">
+				<div className=" w-full h-screen md:px-40 xs:px-6 py-8">
+					<h1 className="md:text-5xl text-4xl font-bold text-stone-900 py-5">
+						Tienda 🐼
+					</h1>
+					<div className="grid sm:grid-cols-3 xs:grid-cols-1 gap-8 place-content-between">
+						{shops.map((shop, index) => {
+							const { nombre, precio, img } = shop;
+							return (
+								<div key={index} className="flex justify-center">
+									<div className="overflow-hidden rounded-lg shadow-lg border bg-zinc-200 p-6">
+										<h3 className="text-stone-900 text-3xl font-bold">
+											{nombre}
+										</h3>
+										<p className="font-serif text-stone-900 font-semibold text-lg py-2">
+											${precio}
+										</p>
+										<div className="opacity-100 hover:opacity-75">
+											<img className="w-80 h-72 rounded-lg" src={img} alt={nombre} />
+										</div>
+									</div>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 		</>
